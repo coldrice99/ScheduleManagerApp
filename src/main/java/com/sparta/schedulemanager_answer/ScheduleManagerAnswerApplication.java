46a17ca471d44c678d540fcbc022f9eac2113b04
coldrice99 <coldrice99@gmail.com>
2024-10-10 11:35:40 +0900

    init commit

---
+++ src/main/java/com/sparta/schedulemanager_answer/ScheduleManagerAnswerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulemanager_answer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagerAnswerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagerAnswerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/schedulemanager_answer/ScheduleManagerAnswerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulemanager_answer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagerAnswerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
